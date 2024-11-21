534276c8a7d24ac2488d908f2e130f52f4508e75
Aman Mansuri <162823789+Mansuriamn@users.noreply.github.com>
2024-11-21 16:45:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,7 @@ let jokesCache = {
 const CACHE_DURATION = 5 * 60 * 1000;
 
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://pokemon-quey.onrender.com']
-}));
+app.use(cors());
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
 // Create MySQL connection pool
